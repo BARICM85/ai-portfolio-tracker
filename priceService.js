504d4c07fffbbcc51639c6243ed347d010602100
BARICM85 <64754982+BARICM85@users.noreply.github.com>
2026-03-06 12:53:40 +0530

    Add async function to fetch live stock price

--- priceService.js
+++ priceService.js
@@ -1,1 +1,20 @@
+async function fetchLivePrice(symbol){
 
+try{
+
+const res = await fetch(
+"https://stock-price-proxy-2owf.onrender.com/price/"+symbol
+);
+
+const data = await res.json();
+
+return data.price;
+
+}
+catch{
+
+return null;
+
+}
+
+}
