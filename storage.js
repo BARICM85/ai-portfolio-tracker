1125bcaa007201f8e9762c9a5db46d1ac41df2c7
BARICM85 <64754982+BARICM85@users.noreply.github.com>
2026-03-06 12:52:51 +0530

    Implement portfolio management functions
    Add functions to manage local storage for portfolios.

--- storage.js
+++ storage.js
@@ -1,1 +1,25 @@
+let activePortfolio = "A";
 
+function getKey() {
+    return activePortfolio === "A" ? "portfolioA" : "portfolioB";
+}
+
+function loadPortfolio() {
+    const data = localStorage.getItem(getKey());
+    return data ? JSON.parse(data) : [];
+}
+
+function savePortfolio(portfolio) {
+    localStorage.setItem(getKey(), JSON.stringify(portfolio));
+}
+
+function clearPortfolio() {
+    localStorage.removeItem(getKey());
+}
+
+function switchPortfolio(type) {
+
+    activePortfolio = type;
+
+    showPortfolio();
+}
