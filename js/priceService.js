5a40561a997f8fdb84efa97818ebd66beff9d516
BARICM85 <64754982+BARICM85@users.noreply.github.com>
2026-03-06 21:06:23 +0530

    Update priceService.js

--- js/priceService.js
+++ js/priceService.js
@@ -1,47 +1,22 @@
-const proxy = "https://corsproxy.io/?";
+const proxy="https://corsproxy.io/?";
 
 export async function fetchPrice(symbol){
 
 try{
 
-const url = proxy + encodeURIComponent(
+const url=proxy+encodeURIComponent(
 `https://query1.finance.yahoo.com/v7/finance/quote?symbols=${symbol}`
 );
 
-const res = await fetch(url);
+const res=await fetch(url);
 
-const data = await res.json();
-
-return data.quoteResponse.result[0].regularMarketPrice;
-
-}
-catch(e){
-
-console.log("Price fetch failed",e);
-return null;
-
-}
-
-}
-
-
-export async function fetchNifty(){
-
-try{
-
-const url = proxy + encodeURIComponent(
-`https://query1.finance.yahoo.com/v7/finance/quote?symbols=%5ENSEI`
-);
-
-const res = await fetch(url);
-
-const data = await res.json();
+const data=await res.json();
 
 return data.quoteResponse.result[0].regularMarketPrice;
 
-}
-catch{
+}catch(e){
 
+console.log(e);
 return null;
 
 }
