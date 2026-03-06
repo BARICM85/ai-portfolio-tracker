ffc87b9c0cb7ef0ba2cfdc66004a350f70ce4325
BARICM85 <64754982+BARICM85@users.noreply.github.com>
2026-03-06 19:48:57 +0530

    Delete js/riskEngine.js

--- js/riskEngine.js
+++
@@ -1,9 +0,0 @@
-export function portfolioVolatility(returns){
-
-let avg=returns.reduce((a,b)=>a+b)/returns.length;
-
-let variance=returns.reduce((a,b)=>a+(b-avg)**2,0)/returns.length;
-
-return Math.sqrt(variance);
-
-}
