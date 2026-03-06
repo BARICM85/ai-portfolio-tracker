5a4e1c8b3e712ea63f40ce9461e7264eb645b25c
BARICM85 <64754982+BARICM85@users.noreply.github.com>
2026-03-06 12:53:16 +0530

    Add portfolio analysis functions for investment tracking

--- portfolioengine.js
+++ portfolioengine.js
@@ -1,1 +1,54 @@
+function calculateInvested(portfolio){
 
+let total = 0;
+
+for(let stock of portfolio){
+
+total += stock.averagePrice * stock.quantity;
+
+}
+
+return total;
+
+}
+
+function calculateCurrent(portfolio){
+
+let total = 0;
+
+for(let stock of portfolio){
+
+total += stock.currentPrice * stock.quantity;
+
+}
+
+return total;
+
+}
+
+function analysePortfolio(portfolio){
+
+let analysis=[];
+
+for(let stock of portfolio){
+
+let invested = stock.averagePrice * stock.quantity;
+let current = stock.currentPrice * stock.quantity;
+
+let gain = current - invested;
+
+let gainPercent = invested>0 ? gain/invested*100 : 0;
+
+analysis.push({
+
+name:stock.script,
+gain,
+gainPercent
+
+});
+
+}
+
+return analysis;
+
+}
