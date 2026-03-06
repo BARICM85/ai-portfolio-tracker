5b35079f721964bf084c22393551f2d7c3644ad1
BARICM85 <64754982+BARICM85@users.noreply.github.com>
2026-03-06 19:11:15 +0530

    Update marketHeatmap.js

--- js/marketHeatmap.js
+++ js/marketHeatmap.js
@@ -1,48 +1,11 @@
-import { fetchMarketData } from "./marketService.js";
+export function showMarketHeatmap(){
 
-const niftyStocks = [
+document.getElementById("heatmap").innerHTML=`
 
-"TCS.NS",
-"RELIANCE.NS",
-"HDFCBANK.NS",
-"ICICIBANK.NS",
-"INFY.NS",
-"ITC.NS",
-"LT.NS",
-"SBIN.NS",
-"AXISBANK.NS"
+<h3>Market Heatmap</h3>
 
-];
+<p>(Future feature: sector heatmap)</p>
 
-export async function showMarketHeatmap(){
-
-let html = "<h2>Market Heatmap</h2>";
-html += '<div class="heatmap">';
-
-for(let symbol of niftyStocks){
-
-let data = await fetchMarketData(symbol);
-
-if(!data) continue;
-
-let color =
-data.changePercent >= 0
-? "green"
-: "red";
-
-html += `
-<div class="heat-cell"
-style="background:${color}">
-${symbol.replace(".NS","")}
-<br>
-${data.changePercent.toFixed(2)}%
-</div>
 `;
-
-}
-
-html += "</div>";
-
-document.getElementById("heatmap").innerHTML = html;
 
 }
