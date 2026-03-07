e033dafb5f627549eb6cdd3adfc32d93de8a2632
BARICM85 <64754982+BARICM85@users.noreply.github.com>
2026-03-07 18:20:52 +0530

    Update marketHeatmap.js

--- js/marketHeatmap.js
+++ js/marketHeatmap.js
@@ -1,6 +1,24 @@
-export function showMarketHeatmap(){
+export function drawHeatmap(portfolio){
 
-document.getElementById("heatmap").innerHTML=
-"<h3>Market Heatmap (Coming Soon)</h3>"
+let html=""
+
+portfolio.forEach(s=>{
+
+let color=s.change>=0?"green":"red"
+
+html+=`<div style="background:${color};
+padding:10px;
+margin:5px;
+color:white;
+display:inline-block">
+
+${s.name}<br>
+${s.change.toFixed(2)}%
+
+</div>`
+
+})
+
+document.getElementById("heatmap").innerHTML=html
 
 }
