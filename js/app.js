e04d8273ea16934ae1b461c62b54ce84e1025be0
bariya chetan <64754982+BARICM85@users.noreply.github.com>
2026-03-07 21:26:20 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,18 +1,29 @@
 import {loadPortfolio,savePortfolio,switchPortfolio,clearPortfolio} from "./storage.js"
+
 import {calculateInvested,calculateCurrent,calculateCAGR,getTopMovers} from "./portfolioEngine.js"
+
 import {fetchPrice,fetchNifty} from "./priceService.js"
+
+import {drawSectorChart} from "./allocation.js"
+
+import {drawHeatmap} from "./marketHeatmap.js"
+
+import {aiSignal} from "./aiAnalysis.js"
 
 import "./excelImport.js"
 
+window.switchPortfolio=switchPortfolio
+window.clearPortfolio=clearPortfolio
+
 window.showDashboard=async function(){
 
-let portfolio=loadPortfolio()
+let p=loadPortfolio()
 
 let nifty=await fetchNifty()
 
 document.getElementById("nifty").innerText=nifty
 
-for(let s of portfolio){
+for(let s of p){
 
 let price=await fetchPrice(s.code)
 
@@ -22,29 +33,35 @@ s.change=((price-s.price)/s.price)*100
 
 }
 
-savePortfolio(portfolio)
+savePortfolio(p)
 
-let invested=calculateInvested(portfolio)
+document.getElementById("invested").innerText=calculateInvested(p)
 
-let current=calculateCurrent(portfolio)
+document.getElementById("current").innerText=calculateCurrent(p)
 
-document.getElementById("invested").innerText=invested
-
-document.getElementById("current").innerText=current
-
-let cagr=calculateCAGR(invested,current,3)
+let cagr=calculateCAGR(calculateInvested(p),calculateCurrent(p),3)
 
 document.getElementById("cagr").innerText=cagr.toFixed(2)+"%"
 
-renderPortfolio(portfolio)
+renderTable(p)
+
+drawSectorChart(p)
+
+drawHeatmap(p)
+
+let movers=getTopMovers(p)
+
+document.getElementById("movers").innerText=
+
+"Gainer: "+(movers.gainer?.name||"-")+" | Loser: "+(movers.loser?.name||"-")
 
 }
 
-function renderPortfolio(portfolio){
+function renderTable(p){
 
 let html=""
 
-portfolio.forEach((s,i)=>{
+p.forEach((s,i)=>{
 
 html+=`
 
@@ -60,11 +77,9 @@ html+=`
 
 <td>${s.change.toFixed(2)}%</td>
 
-<td>
+<td>${aiSignal(s)}</td>
 
-<button onclick="deleteStock(${i})">Delete</button>
-
-</td>
+<td><button onclick="deleteStock(${i})">Delete</button></td>
 
 </tr>
 
@@ -78,11 +93,11 @@ document.getElementById("tableBody").innerHTML=html
 
 window.deleteStock=function(i){
 
-let portfolio=loadPortfolio()
+let p=loadPortfolio()
 
-portfolio.splice(i,1)
+p.splice(i,1)
 
-savePortfolio(portfolio)
+savePortfolio(p)
 
 showDashboard()
 
@@ -96,11 +111,13 @@ document.getElementById("content").innerHTML=`
 
 <input id="name" placeholder="Name">
 
-<input id="code" placeholder="Symbol (RELIANCE.NS)">
+<input id="code" placeholder="Symbol">
 
 <input id="price" placeholder="Buy Price">
 
 <input id="qty" placeholder="Qty">
+
+<input id="sector" placeholder="Sector">
 
 <button onclick="addStock()">Add</button>
 
@@ -110,27 +127,26 @@ document.getElementById("content").innerHTML=`
 
 window.addStock=function(){
 
-let portfolio=loadPortfolio()
+let p=loadPortfolio()
 
-let stock={
+p.push({
 
 name:document.getElementById("name").value,
+
 code:document.getElementById("code").value,
+
 price:Number(document.getElementById("price").value),
-qty:Number(document.getElementById("qty").value)
 
-}
+qty:Number(document.getElementById("qty").value),
 
-portfolio.push(stock)
+sector:document.getElementById("sector").value
 
-savePortfolio(portfolio)
+})
+
+savePortfolio(p)
 
 showDashboard()
 
 }
-
-window.switchPortfolio=switchPortfolio
-
-window.clearPortfolio=clearPortfolio
 
 showDashboard()
